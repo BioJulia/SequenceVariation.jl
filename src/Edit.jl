b36ab69c728bf287219e5ff9fea1926cb0cdb7ae
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2022-12-30 14:38:59 -0600

    Move Deletion-related code to edits/Deletion.jl

--- src/Edit.jl
+++ src/Edit.jl
@@ -1,22 +1,5 @@
 include("edits/Substitution.jl")
-
-"""
-    Deletion
-
-Represents the deletion of N symbols. The location of the deletion is stored
-outside this struct
-"""
-struct Deletion
-    len::UInt
-
-    function Deletion(len::UInt)
-        iszero(len) && error("Deletion must be at least 1 symbol")
-        return new(len)
-    end
-end
-Deletion(x::Integer) = Deletion(convert(UInt, x))
-Base.length(x::Deletion) = Int(x.len)
-Base.hash(x::Deletion, h::UInt) = hash(Deletion, hash(x.len, h))
+include("edits/Deletion.jl")
 
 """
     Insertion{S <: BioSequence}
@@ -93,22 +76,6 @@ end
 function lendiff(edit::Edit)
     x = edit.x
     return x isa Substitution ? 0 : (x isa Deletion ? -length(x) : length(x.x))
-end
-
-function _refbases(d::Deletion, reference::S, pos::UInt) where {S<:BioSequence}
-    if pos == 1
-        return S(reference[UnitRange{Int}(pos, pos + length(d))])
-    else
-        return S(reference[UnitRange{Int}(pos - 1, pos + length(d) - 1)])
-    end
-end
-
-function _altbases(d::Deletion, reference::S, pos::UInt) where {S<:BioSequence}
-    if pos == 1
-        return S([reference[pos + 1]])
-    else
-        return S([reference[pos - 1]])
-    end
 end
 
 function _refbases(i::Insertion, reference::S, pos::UInt) where {S<:BioSequence}
