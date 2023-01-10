BioJulia/SequenceVariation.jl

commit da2cbbd5282b93b6c8399c943734057fa55d61f0

Add equals functionality to Deletion

src/edits/Deletion.jl
@@ -15,6 +15,7 @@ end
 Deletion(x::Integer) = Deletion(convert(UInt, x))
 
 Base.length(x::Deletion) = Int(x.len)
+Base.:(==)(x::Substitution, y::Substitution) = length(x) == length(y)
 Base.hash(x::Deletion, h::UInt) = hash(Deletion, hash(x.len, h))
 
 function _refbases(d::Deletion, reference::S, pos::UInt) where {S<:BioSequence}

commit 2ff4181dd8a4f5fc173d2b694194cac9fa690d21

Fix deletion equality function
Fixes: da2cbbd5282b ("Add equals functionality to Deletion")

Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

src/edits/Deletion.jl
@@ -15,7 +15,7 @@ end
 Deletion(x::Integer) = Deletion(convert(UInt, x))
 
 Base.length(x::Deletion) = Int(x.len)
-Base.:(==)(x::Substitution, y::Substitution) = length(x) == length(y)
+Base.:(==)(x::Deletion, y::Deletion) = length(x) == length(y)
 Base.hash(x::Deletion, h::UInt) = hash(Deletion, hash(x.len, h))
 
 function _refbases(d::Deletion, reference::S, pos::UInt) where {S<:BioSequence}